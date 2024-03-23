f1b06be75b77f3dab136b9457e34389f3b1d583e
raythestudent-maisonneuve <70280810+Raythestudent-momo@users.noreply.github.com>
2024-03-23 14:07:58 -0400

    s7c2 - affichage des catégories
    Changement du dégradé de l'entête, début de l'affichage des catégories et ajout d'une couleur pour le menu.

--- front-page.php
+++ front-page.php
@@ -21,11 +21,8 @@
     </div>
     <div id="accueil" class="global">
         <section class="accueil__section">
-            <h2>Accueil (h2)</h2>
-        <div class="section__carte">  
- <?php
-
-  ?>      
+            <h2>Destinations populaires</h2>
+        <div class="section__carte">       
   <?php if (have_posts()):
         while(have_posts()): the_post(); ?>
         <div class="carte">
@@ -37,7 +34,7 @@
        <?php endwhile; ?>
     <?php endif; ?>
   </div>
-
+        </section>
     </div>
     <div id="galerie" class="global diagonal">
         <section class="galerie__section">
@@ -48,16 +45,32 @@
     </div>
     <div id="evenement" class="global">
         <section class="evenement__section">
-            <h2>Événement</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#">Lorem, ipsum.</a>  Minima velit qui unde odit quae, <a href="#">Lorem, ipsum.</a>  magni labore maiores facilis obcaecati dolore, ullam facere. Ducimus veniam reprehenderit, temporibus ab at possimus fugit?</p>
-             <blockquote>Événement ipsum, dolor sit amet consectetur adipisicing elit. Accusantium a, repellat alias qui ut in ratione optio quia quae minus repudiandae ducimus aliquid aperiam unde atque tempore non. Non, magnam.</blockquote>
-            <!--
-                Avec wordpress dans le fichier front-page.php, je voudrais faire afficher des cartes de catégorie. Chacune des carte contiendrait: le nom de la catégorie,
-                les 10 premiers mots de la description de la catégorie, le nombre d'article dans la catégorie et un lien vers une page category contenant une liste d'articles 
-                de cette catégorie.  
-            -->
+            <h2>Catégories</h2>
+            <div class="section__carte">
+            <?php
+                $categories = get_categories();
 
+                foreach ($categories as $category) {
+                    $category_link = get_category_link($category->term_id);
+                    $description = wp_trim_words($category->description, 10, '...');
+                    ?>
+
+                    <div class="carte">
+                        <h2><a href="<?php echo esc_url($category_link); ?>"><?php echo $category->name; ?></a></h2>
+                        <p><?php echo $description; ?></p>
+                        <p><?php echo $category->count . 'articles'; ?></p>
+                        <a href="<?php echo esc_url($category_link); ?>">Voir la catégorie</a>
+                    </div>
+            <?php
+            }
+            ?>
+            </div>
         </section>
+<!--
+    Avec wordpress dans le fichier front-page.php, je voudrais faire afficher des cartes de catégorie. Chacune des carte contiendrait: le nom de la catégorie,
+    les 10 premiers mots de la description de la catégorie, le nombre d'article dans la catégorie et un lien vers une page category contenant une liste d'articles 
+    de cette catégorie.  
+-->
         <?php get_template_part("gabarit/vague"); ?>
     </div>
 
