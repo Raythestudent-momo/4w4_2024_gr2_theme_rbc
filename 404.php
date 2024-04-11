1770ba59aaa00cb97ee36ea372b9bd268ecf432c
raythestudent-maisonneuve <70280810+Raythestudent-momo@users.noreply.github.com>
2024-04-11 16:01:00 -0400

    s9c2 - confusion totale, je sais pas ce que je fais

--- 404.php
+++ 404.php
@@ -11,9 +11,26 @@
             <h1>Erreur 404</h1>
             <h3>Vous essayez d'accéder à une page qui n'existe pas</h3>
             <h3>Pour revenir à la page d'accueil, cliquez sur le lien suivant</h3>
+            <div class="entete__header__button"><button href="http://localhost:8080/4W4_wp/" class="entete__button">Retour à l'accueil</button></div>
+            <?php get_search_form(); ?>
+            <div class="section__carte">
+            <?php
+                $categories = get_categories();
+
+                foreach ($categories as $category) {
+                    $category_link = get_category_link($category->term_id);
+                    ?>
+
+                    <div class="sociaux">
+                        <p><a href="<?php echo esc_url($category_link); ?>"><?php echo $category->name; ?></a></h2>
+                    </div>
+            <?php
+            }
+            ?>
+            </div>
         </section>
         <section>
-            <img class="404__image" src="http://localhost:8080/4W4_wp/wp-content/uploads/2024/03/potato-150-px-2.png" alt="">
+            <img class="erreur__image" src="http://localhost:8080/4W4_wp/wp-content/uploads/2024/03/potato-150-px-2.png" alt="">
         </section>
     </div>
 <!--
